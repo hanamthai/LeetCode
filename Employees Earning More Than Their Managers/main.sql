09e4032dcf2fe40ba286daf53e58a0909ba2aced
ThaiNamHa-windows <hanamthai02@gmail.com>
2024-02-11 13:51:43 +0700

    Employees Earning More Than Their Managers

---
+++ Employees Earning More Than Their Managers/main.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+-- Write your PostgreSQL query statement below
+select e1.name as Employee from Employee e1
+join Employee e2 on e1.managerId = e2.id
+where e1.salary > e2.salary
