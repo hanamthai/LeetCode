b7b21ece2ffb9c255e106c6d6d2dc8aa89f22408
ThaiNamHa-windows <hanamthai02@gmail.com>
2024-02-15 12:48:01 +0700

    Delete Duplicate Emails v1

--- Delete Duplicate Emails/main.sql
+++ Delete Duplicate Emails/main.sql
@@ -4,3 +4,11 @@ delete from Person p1
 using Person p2
 where p1.id > p2.id and p1.email = p2.email;
 select * from Person
+
+delete from Person
+where id in (
+    select id from (
+        select id, row_number() over(partition by email order by id asc) as row_num
+        from Person
+    ) p1 where p1.row_num > 1
+)
