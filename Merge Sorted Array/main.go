e405f5af872ea182dbca70e9f3f6e2ecfb39529a
ThaiNamHa-windows <hanamthai02@gmail.com>
2024-02-25 23:36:19 +0700

    Merge Sorted Array

---
+++ Merge Sorted Array/main.go
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/merge-sorted-array/
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+    nums1 = nums1[:m]
+    nums1 = append(nums1, nums2...)
+    sort.Slice(nums1, func(i, j int) bool {
+        return nums1[i] < nums1[j]
+    })
+}
